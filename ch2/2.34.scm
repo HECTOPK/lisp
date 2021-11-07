2859d715bf136175ddf5f968df85ec37f85e000e
Nestor P <hectopk@gmail.com>
2021-11-07 22:40:11 +0300

    2.34

---
+++ ch2/2.34.scm
@@ -0,0 +1,9 @@
+(define (accumulate op init seq)
+	(if (null? seq)
+		init	
+		(op (car seq) (accumulate op init (cdr seq)))))
+
+(define (horner-eval x seq)
+	(accumulate (lambda (this higher) (+ (* x higher) this)) 0 seq))
+
+(horner-eval 2 (list 1 3 0 5 0 1)) ; 79
