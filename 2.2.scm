1ceb7586c0c19752654f743b3cd5e2c3b466a083
Nestor P <hectopk@gmail.com>
2021-11-07 16:35:46 +0300

    2.2

--- 2.2.scm
+++ 2.2.scm
@@ -12,23 +12,11 @@
 (define (y-point p) (cdr p))
 
 
-(define (make-rect ab cd) (cons ab cd))
-
-(define (a-point p) (car (car p)))
-
-(define (b-point p) (cdr (car p)))
-
-(define (c-point p) (car (cdr p)))
-
-(define (d-point p) (cdr (cdr p)))
-
-(define (square r) (x-point (a-point r)) (a-point r))
-
 (define (print-point p)
   (newline)
   (display "(")
   (display (x-point p))
-  (display ",")
+  (display ", ")
   (display (y-point p))
   (display ")"))
 
@@ -38,6 +26,4 @@
 		(/ (+ (y-point (start-segment s)) (y-point (end-segment s))) 2)
 	))
 
-; (print-point (midpoint-segment (make-segment (make-point 0 0) (make-point 1 1)))
-; (define mp (midpoint-segment (make-segment (make-point 0 0 ) (make-point 1 1))))
-; (print-point (midpoint-segment (make-segment (make-point 0 10 ) (make-point 1 1))))
+(print-point (midpoint-segment (make-segment (make-point 0 0) (make-point 2 2))))
