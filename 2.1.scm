d2de3f19cc007eb022f3921ed717ca24db78aa74
Nestor P <hectopk@gmail.com>
2021-11-07 16:35:24 +0300

    2.1

--- 2.1.scm
+++ 2.1.scm
@@ -1,3 +1,8 @@
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
 
 (define (add-rat x y)
   (make-rat (+ (* (numer x) (denom y))
@@ -21,78 +26,16 @@
   (= (* (numer x) (denom y))
      (* (numer y) (denom x))))
 
-;: (define x (cons 1 2))
-;: 
-;: (car x)
-;: (cdr x)
-
-;: (define x (cons 1 2))
-;: (define y (cons 3 4))
-;: (define z (cons x y))
-;: (car (car z))
-;: (car (cdr z))
-
-(define (make-rat-b n d)
-  (if (* n d) < 0)
-  )
-
-(define (make-rat n d) (cons n d))
-
-(define (numer x) (car x))
-
-(define (denom x) (cdr x))
-
-;;footnote -- alternative definitions
-(define make-rat cons)
-(define numer car)
-(define denom cdr)
-
-(define (print-rat x)
-  (newline)
-  (display (numer x))
-  (display "/")
-  (display (denom x)))
-
-
-;: (define one-half (make-rat 1 2))
-;: 
-;: (print-rat one-half)
-;: 
- (define one-third (make-rat 1 3))
-;: 
-;: (print-rat (add-rat one-half one-third))
-;: (print-rat (mul-rat one-half one-third))
-;: (print-rat (add-rat one-third one-third))
-
-
-;; reducing to lowest terms in constructor
-;; (uses gcd from 1.2.5 -- see ch2support.scm)
-(define (gcd a b)
-  (if (= b 0)
-      a
-      (gcd b (remainder a b))))
-
-(define (norm x)
-  (if (> x 0) 
-    x
-    (- x)))
-
-(define (make-rat-b n d)
- ; (let ((g (norm n)))
-    (cond 
-      ((and (>= n 0) (> d 0)) (make-rat n d))
-      ((and (>= n 0) (< d 0)) (make-rat (- n) (- d)))
-      ((and (<  n 0) (> d 0)) (make-rat n d))
-      ((and (<  n 0) (< d 0)) (make-rat (- n) (- d)))
-    ; )
-    ; (cons (/ n g) (/ d g))
-    ))
+(define (sign n) (/ n (abs n)))
 
 (define (make-rat n d)
-  (let ((g (norm(gcd n d))))
+  (let (
+    (g (abs (gcd n d)))
+    (s (sign (* n d))))
+    (cons (* s (/ (abs n) g)) (/ (abs d) g))))
 
-    (cons (/ n g) (/ d g))))
+(define (numer n) (car n))
+(define (denom n) (cdr n))
 
-(print-rat (make-rat-b 1 -2))
+(print-rat (make-rat 1 2))
 
- ; (print-rat (add-rat one-third one-third))
