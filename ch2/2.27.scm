3e6e2cc35d3ae270cd4eda0172c51b386ff9b4a5
Nestor P <hectopk@gmail.com>
2021-11-07 18:35:01 +0300

    2.27

--- ch2/2.27.scm
+++ ch2/2.27.scm
@@ -1,33 +1,8 @@
-(define (count-leaves x)
-  (cond ((null? x) 0)
-        ((not (pair? x)) 1)
-        (else (+ (count-leaves (car x))
-                 (count-leaves (cdr x))))))
+(define (deep-reverse l)
+	(define (iter l r)
+		(cond ((null? l) r)
+			((not (pair? (car l))) (iter (cdr l) (cons (car l) r)))
+			(else (iter (cdr l) (cons (deep-reverse (car l)) r)))))
+	(iter l '()))
 
-(define (append list1 list2)
-  (if (null? list1)
-      list2
-      (cons (car list1) (append (cdr list1) list2))))
-
-(define x (list (list 1 2) (list 3 4)))
-
-(define (deep-reverse-iter x r)
-	(display x)
-	(display r)
-	(newline)
-	(cond ((null? x) x)
-		  (else (append (deep-reverse (cdr x))  (deep-reverse (car x))))))
-
-(define (deep-reverse x) 
-	(display x)
-	(newline)
-	(cond ((null? x) x)
-		  ((not (pair? x))  x)
-		  ((null? (cdr x)) (list (deep-reverse (car x))))
-	      (else (cons (deep-reverse (cdr x))  (deep-reverse (car x))))))
-	; (if (not (pair? x))
-	; 	x
-	; 	((deep-reverse-iter x (list)))))
-
-(deep-reverse x)
-(count-leaves x)
+(deep-reverse (list (list 1 2) (list 3 4))) ; ((4 3) (2 1))
