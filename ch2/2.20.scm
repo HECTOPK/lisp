112c6804a8bb74f73a57fff0f05ba12a2b81fd8d
Nestor P <hectopk@gmail.com>
2021-11-07 16:55:35 +0300

    2.20

--- ch2/2.20.scm
+++ ch2/2.20.scm
@@ -1,45 +1,22 @@
-
 (define (reverse l)
-	(reverse-iter (cdr l) (list (car l))))
+	(define (iter l r)
+    (if (null? l)
+      r
+      (iter (cdr l) (cons (car l) r))))
+  (iter l '()))
 
-
-(define (reverse-iter l n) 
-	(newline)
-	(display l)
-	(display n)
-	(newline)
-	(if (null? l)
-		n
-		(reverse-iter (cdr l) (cons (car l) n))))
-
-
-(define (gcd a b)
-  (if (= b 0)
-      a
-      (gcd b (remainder a b))))
-
-
-
-(define (odd? n) (= (gcd n 2) 1))
-(define (even? n) (= (gcd n 2) 2))
+(define (odd? n) (= (remainder n 2) 1))
+(define (even? n) (= (remainder n 2) 0))
 
 (define (same-parity n . l)
-	(if (odd? n)
-		(reverse (same-parity-iter odd? l (list n)))
-		(reverse (same-parity-iter even? l (list n)))))
+  (define check? (if (odd? n) odd? even?))
+  (define (iter l r)
+    (if (null? l)
+      r
+      (if (check? (car l))
+        (iter (cdr l) (cons (car l) r))
+        (iter (cdr l) r))))
+  (reverse (iter l (list n))))
 
-
-(define (same-parity-iter checker l r) 
-	(newline)
-	(display l)
-	(display r)
-	(newline)
-	(if (null? l)
-		r
-		(if (checker (car l))
-			(same-parity-iter checker (cdr l) (cons (car l) r))
-			(same-parity-iter checker (cdr l) r))))
-
-
-(same-parity 1 2 3 4 5 6 7)
-(same-parity 2 3 4 5 6 7)
+(same-parity 1 2 3 4 5 6 7) ; (1 3 5 7)
+(same-parity 2 3 4 5 6 7) ; (2 4 6)
