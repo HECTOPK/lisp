25acd2ad052e37633bdacc4947f8229fa50fe93e
Nestor P <hectopk@gmail.com>
2021-11-07 16:34:53 +0300

    2.17

--- 2.17.scm
+++ 2.17.scm
@@ -1,21 +1,6 @@
 (define (last-pair l) 
 	(if (null? (cdr l))
-		(car l)
+		l
 		(last-pair (cdr l))))
 
 (last-pair (list 23 72 149 34))
-
-(define (reverse l)
-	(reverse-iter (cdr l) (list (car l))))
-
-
-(define (reverse-iter l n) 
-	(newline)
-	(display l)
-	(display n)
-	(newline)
-	(if (null? l)
-		n
-		(reverse-iter (cdr l) (cons (car l) n))))
-
-(reverse (list 1 4 9 16 25))
