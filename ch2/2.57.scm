1790c40f1cd6c9d180e49d328d20ab5247c99807
Nestor P <hectopk@gmail.com>
2021-11-09 20:57:57 +0300

    2.57

---
+++ ch2/2.57.scm
@@ -0,0 +1,88 @@
+(define (deriv exp var)
+(newline)
+(display exp)
+(cond ((number? exp) 0)
+      ((variable? exp)
+       (if (same-variable? exp var) 1 0))
+      ((sum? exp)
+       (make-sum (deriv (addend exp) var)
+                 (deriv (augend exp) var)))
+      ((product? exp)
+       (make-sum
+         (make-product (multiplier exp)
+                       (deriv (multiplicand exp) var))
+         (make-product (deriv (multiplier exp) var)
+                       (multiplicand exp))))
+      ((exponentiation? exp)
+      (make-product
+        (exponent exp)
+        (make-product 
+          (make-exponentiation (- (base exp) 1))
+          (deriv (base exp)))))
+      (else
+       (error "unknown expression type -- DERIV" exp))))
+
+;; representing algebraic expressions
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+(and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (sum? x)
+(and (pair? x) (eq? (car x) '+)))
+
+(define (addend s) (cadr s))
+
+(define (augend s) 
+  (if (null? (cdddr s))
+    (caddr s)
+    (cons '+ (cddr s)))
+)
+
+(define (product? x)
+(and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier p) (cadr p))
+
+(define (multiplicand p) 
+  (if (null? (cdddr p))
+    (caddr p)
+    (cons '* (cddr p))))
+
+(define (make-sum a1 a2)
+(cond ((=number? a1 0) a2)
+      ((=number? a2 0) a1)
+      ((and (number? a1) (number? a2)) (+ a1 a2))
+      (else (list '+ a1 a2))))
+
+(define (=number? exp num)
+(and (number? exp) (= exp num)))
+
+(define (make-product m1 m2)
+(cond ((or (=number? m1 0) (=number? m2 0)) 0)
+      ((=number? m1 1) m2)
+      ((=number? m2 1) m1)
+      ((and (number? m1) (number? m2)) (* m1 m2))
+      (else (list '* m1 m2))))
+
+
+(define (exponentiation?  exp)
+  (and (pair? exp) (eq? (car exp) '**)))
+
+(define (base exp)
+  (cadr exp))
+
+(define (exponent exp)
+  (caddr exp))
+
+(define (make-exponentiation base exp)
+  (cond 
+    ((=number? base 1) 1)
+    ((=number? base 0) 0)
+    ((=number? exp 1) exp)
+    ((=number? exp 0) 1)
+    (else (list '** base exp))))
+
+
+(deriv '(* x x x) 'x) ; (1 + (* 2 (* x x)))
